59cefa7ca9bd18a13d6bade87c17f7c9e38db0f3
kaxxsh <kameshwaran2002@gmail.com>
2023-04-06 19:21:36 +0530

    update

--- src/Components/LeftMain/Home.jsx
+++ src/Components/LeftMain/Home.jsx
@@ -52,30 +52,48 @@ function Home({ setNav }) {
           <div className="Recently-Played">
             <div className="Recent-Box">
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://igimages.gumlet.io/tamil/home/naanevaruven300922_4.jpg?w=700&dpr=1.4"
+                  alt=""
+                />
+                <div className="Music-Name">Dhanush</div>
               </div>
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://static.toiimg.com/thumb/74220144.cms?width=680&height=512&imgsize=166081"
+                  alt=""
+                />
+                <div className="Music-Name">Adthi</div>
               </div>
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://c.saavncdn.com/113/Yuvan-Shankar-Raja-Hits-Tamil-2005-20201029224137-500x500.jpg"
+                  alt=""
+                />
+                <div className="Music-Name">U1 Drugs</div>
               </div>
             </div>
             <div className="Recent-Box">
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://static.toiimg.com/thumb/msid-81567469,imgsize-66554,width-800,height-600,resizemode-75/81567469.jpg"
+                  alt=""
+                />
+                <div className="Music-Name">U1</div>
               </div>
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://2.bp.blogspot.com/-YC8THDtFtxE/XLlqBKERZII/AAAAAAAAexE/jCFlbuYr78sei0Jg2eKUejxW1Kf8KU3cgCLcBGAs/s1600/raja.JPG"
+                  alt=""
+                />
+                <div className="Music-Name">U1 Hits</div>
               </div>
               <div className="Played-Box">
-                <div className="Music-Img"></div>
-                <div className="Music-Name">Movie</div>
+                <img
+                  src="https://www.liveclefs.com/wp-content/uploads/2022/12/Pradeep-Kumar.jpg"
+                  alt=""
+                />
+                <div className="Music-Name">Pradeep</div>
               </div>
             </div>
           </div>
